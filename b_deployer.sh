b39e9c61a23b93496adb445a1dadc79e751be48c
eternity6 <sandro@ms3.it>
2021-11-29 16:55:09 +0100

    modificate le sed

--- b_deployer.sh
+++ b_deployer.sh
@@ -6,11 +6,14 @@ basedetail=""
 b_environment=""
 joomlapath=""
 # Sed in action
-sed -i -e "/.*{basepath}*./ s/.*/RewriteBase $basepath/" $joomlapath/b/.htaccess
+sed -i -e "/.*{basepath}*./ s/.*/RewriteBase $basepath\//" $joomlapath/b/.htaccess
 sed -i -e "/.*{baseb}*./ s/.*/RewriteRule ^\([^\\.]+\)\.png$  $baseb\/b\/index.php?code=\$1 [NC,L]/" $joomlapath/b/.htaccess
 sed -i -e "/.*{basedetail}*./ s/.*/RewriteRule ^\([^\\.]+\)\.html$   $basedetail\/b\/dettaglio.php?param=\$1 [NC,L]/" $joomlapath/b/.htaccess
-sed -i -e "/.*{basepath}*./ s/.*/RewriteBase $basepath\/b\/qrcode\/" $joomlapath/b/qrcode/.htaccess
-sed -i -e "/.*{basedetail}*./ s/.*/RewriteRule ^\([^\\.]+\)\.png$   $basedetail\/b\/qrcode\/index.php?code=\$1 [NC,L]" $joomlapath/b/qrcode/.htaccess
+
+
+
+sed -i -e "/.*{qr_code_bpath}*./ s/.*/RewriteBase $basepath\/b\/qrcode\//" $joomlapath/b/qrcode/.htaccess
+sed -i -e "/.*{qr_code_btail}*./ s/.*/RewriteRule ^\([^\\.]+\)\.png$   $basedetail\/b\/qrcode\/index.php?code=\$1 [NC,L]/" $joomlapath/b/qrcode/.htaccess
 sed -i -e "/.*{b_environment}*./ s/.*/define\('environment', '$b_environment'\);/" $joomlapath/b/include/config.inc.php
 
 
